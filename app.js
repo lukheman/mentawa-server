2d5d736a5fd2dd6083c65b44bb9151bf09c6ada2
Akmal <akmalthemalong@gmail.com>
2024-12-23 18:47:51 +0800

    add: userinformation handler

--- app.js
+++ app.js
@@ -25,6 +25,43 @@ app.post('/tokenvalidation', async (req, res) => {
 
 })
 
+app.post('/userinformation', async (req, res) => {
+
+    const machineId = req.body.machineId
+
+    try {
+        const user = await userGetByMachineId(machineId)
+
+        if (!user) {
+            res.status(404).send({
+                status: 'error',
+                message: 'machineId not found'
+            })
+
+        }
+
+        res.status(200).send({
+            status: 'success',
+            data: {
+                user
+            }
+        })
+
+    } catch (error) {
+
+        console.error(error)
+
+        res.status(500).send({
+            status: 'error',
+            message: 'an internal server error',
+            details: error.message
+        })
+
+    }
+
+
+})
+
 app.listen(port, () => {
     console.log('listing at localhost:' + port)
 })

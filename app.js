a23c71f414d8b55e3e6c5438eee3c6fe1d84b64f
Akmal <akmalthemalong@gmail.com>
2024-12-23 15:35:46 +0800

    add: tokenValidation handler

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express')
-
 const port = 3000
 const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+
+const { tokenValidation } = require('./utils/utils.js')
 
 app.post('/gettoken', (req, res) => {
     const machineId = req.body.machineId
 })
 
 app.post('/signup', (req, res) => {
+
+})
+
+app.post('/tokenvalidation', async (req, res) => {
+
+    const machineId = req.body.machineId
+    const token = req.body.token
+
+    const result = await tokenValidation(machineId, token)
+
+    res.json(result)
 
 })
 
